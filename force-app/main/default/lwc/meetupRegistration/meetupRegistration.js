60da263fd31775260e086b86953ace4f22d1f66a
Nolan Cretney <nokynokes@gmail.com>
2024-05-12 17:56:24 -0600

    adding styling and form to lwc

--- force-app/main/default/lwc/meetupRegistration/meetupRegistration.js
+++ force-app/main/default/lwc/meetupRegistration/meetupRegistration.js
@@ -1,12 +1,17 @@
 import { LightningElement, wire, track } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
 import getMeetupByRegistrationCode from '@salesforce/apex/MeetupRegistrationController.getMeetupByRegistrationCode';
+import MEETUP_REGISTRATION_OBJECT from '@salesforce/schema/MeetupRegistration__c';
+import EMAIL_FIELD from '@salesforce/schema/MeetupRegistration__c.Email__c'; 
+import FIRST_NAME_FIELD from '@salesforce/schema/MeetupRegistration__c.FirstName__c';
+import LAST_NAME_FIELD from '@salesforce/schema/MeetupRegistration__c.LastName__c';
+import MEETUP_FIELD from '@salesforce/schema/MeetupRegistration__c.Meetup__c';   
 
 export default class MeetupRegistration extends LightningElement {
+    meetupRegistrationObj = MEETUP_REGISTRATION_OBJECT;
+    meetupRegistrationFields = [EMAIL_FIELD, FIRST_NAME_FIELD, LAST_NAME_FIELD];
+    
     @track registrationCode;
-    @track meetup;
-    @track meetup;
-    @track error;
 
     @wire(CurrentPageReference)
     setPageReference(currentPageReference) {
@@ -16,15 +21,25 @@ export default class MeetupRegistration extends LightningElement {
     }
 
     @wire(getMeetupByRegistrationCode, { registrationCode: '$registrationCode' })
-    wiredMeetup({ data, error }){
-        console.log(data);
-        console.log(error);
-        if (data) {
-            this.meetup = data;
-            this.error = undefined;
-        } else if (error) {
-            this.meetup = undefined;
-            this.error = error;
-        }
+    meetup;
+
+    get meetupName() {
+        return this.meetup.data.Name;
+    }
+
+    get meetupRegistrationCode() {
+        return this.meetup.data.RegistrationCode__c;
+    }
+
+    get meetupRegistrationLimit() {
+        return this.meetup.data.RegistrationLimit__c;
+    }
+
+    get meetupRegistrationStatus() {
+        return this.meetup.data.Status__c;
+    }
+
+    get error() {
+        return this.meetup.error;
     }
 }
