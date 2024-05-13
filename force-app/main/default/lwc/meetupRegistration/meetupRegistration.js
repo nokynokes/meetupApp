60427ddc39297b545bc087d8fcf4e7dfe7ce89aa
Nolan Cretney <nokynokes@gmail.com>
2024-05-13 14:06:50 -0600

    add option to search for other meetups

--- force-app/main/default/lwc/meetupRegistration/meetupRegistration.js
+++ force-app/main/default/lwc/meetupRegistration/meetupRegistration.js
@@ -2,16 +2,24 @@ import { LightningElement, wire } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getMeetupByRegistrationCode from '@salesforce/apex/MeetupRegistrationController.getMeetupByRegistrationCode';
+import getCode from '@salesforce/apex/MeetupRegistrationController.getCode';
+
 import MEETUP_REGISTRATION_OBJECT from '@salesforce/schema/MeetupRegistration__c';
 import EMAIL_FIELD from '@salesforce/schema/MeetupRegistration__c.Email__c'; 
 import FIRST_NAME_FIELD from '@salesforce/schema/MeetupRegistration__c.FirstName__c';
-import LAST_NAME_FIELD from '@salesforce/schema/MeetupRegistration__c.LastName__c';   
+import LAST_NAME_FIELD from '@salesforce/schema/MeetupRegistration__c.LastName__c'; 
+import MEETUP_FIELD from '@salesforce/schema/MeetupRegistration__c.Meetup__c';   
 
 export default class MeetupRegistration extends LightningElement {
     meetupRegistrationObj = MEETUP_REGISTRATION_OBJECT;
     meetupRegistrationFields = [EMAIL_FIELD, FIRST_NAME_FIELD, LAST_NAME_FIELD];
+    emailField = EMAIL_FIELD;
+    firstNameField = FIRST_NAME_FIELD;
+    lastNameField = LAST_NAME_FIELD;
+    meetupField = MEETUP_FIELD;
     
     registrationCode;
+    meetupId;
 
     @wire(CurrentPageReference)
     setRegistrationCodeFromURL(currentPageReference) {
@@ -31,6 +39,23 @@ export default class MeetupRegistration extends LightningElement {
         });
     }
 
+    handleSearchSubmit(event) {
+        console.log('hi')
+        event.preventDefault();
+        const id = event.detail.fields.Meetup__c;
+        console.log(id);
+
+        getCode({ search: id })
+            .then((results) => {
+                this.registrationCode = results;    
+            })
+            .catch(console.log)
+    }
+
+    handleClick(event) {
+        this.meetup = null;
+    }
+
     handleSuccess(event) {
         this.dispatchEvent(new ShowToastEvent({
             title: 'Registration Complete!',
@@ -43,27 +68,15 @@ export default class MeetupRegistration extends LightningElement {
         return this.meetup.data.Name;
     }
 
-    get meetupRegistrationCode() {
-        return this.meetup.data.RegistrationCode__c;
+    get meetupId() {
+        return this.meetup.data.Id;
     }
 
-    get meetupRegistrationCount() {
-        return this.meetup.data.RegistrationCount__c;
-    }
-
-    get meetupRegistrationLimit() {
-        return this.meetup.data.RegistrationLimit__c;
-    }
-
-    get meetupRegistrationStatus() {
-        return this.meetup.data.Status__c;
-    }
-
-    get limitNotReached() {
-        return this.meetup.data.RegistrationCode__c !== this.meetup.data.RegistrationLimit__c;
+    get data() {
+        return this.meetup?.data;
     }
 
     get error() {
-        return this.meetup.error;
+        return this.meetup?.error;
     }
 }
